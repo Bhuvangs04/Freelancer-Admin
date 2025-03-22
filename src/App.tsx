e24a72e8576153271b52413960c62494dc2c3420
Bhuvan G Sangappanavar <bhuvangs2004@gmail.com>
2025-03-22 13:58:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import AdminPage from "./pages/admin/Index";
 import NotFound from "./pages/NotFound";
 
@@ -17,8 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/" element={<AdminPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
